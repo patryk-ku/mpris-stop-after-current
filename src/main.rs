2f871531ffd472a0a5652d2f90d4825429b7b653
Patryk Kurdziel <patryk.kurdziel@protonmail.com>
2025-01-06 12:19:22 +0100

    feat: better error handling and improve code readability

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,48 @@
 use mpris::{Event, PlayerFinder};
 use std::env;
+use std::error::Error;
 use tokio::task;
 use tokio::time::{timeout, Duration};
 
-fn stop_after_current() {
+fn stop_after_current() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let player;
-
-    if args.len() > 1 {
+    let player = if args.len() > 1 {
         let argument = &args[1];
         println!("Player name from args: {}", argument);
-        player = PlayerFinder::new()
-            .expect("Could not connect to D-Bus.")
-            .find_by_name(argument)
-            .expect("Could not find active player.");
+        PlayerFinder::new()?.find_by_name(argument)?
     } else {
-        player = PlayerFinder::new()
-            .expect("Could not connect to D-Bus.")
-            .find_active()
-            .expect("Could not find active player.");
-        println!("You can specify which player to control by providing its name as an argument.")
-    }
+        println!("You can specify which player to control by providing its name as an argument.");
+        PlayerFinder::new()?.find_active()?
+    };
 
     println!("Selected player: {}", player.identity());
     println!("\"Stop after current track\" - enabled for next 10 min.");
     println!("Waiting for track change... (Exit with Ctrl-C)");
 
-    let events = player.events().expect("Could not start event stream.");
-
+    let events = player.events()?;
     for event in events {
+        // println!("{:#?}", event);
         match event {
-            Ok(event) => {
-                // println!("{:#?}", event);
-                if matches!(event, Event::PlayerShutDown) {
-                    println!("Player has ben shut down.");
-                    break;
-                }
-
-                if matches!(event, Event::TrackChanged(_)) {
-                    match player.pause() {
-                        Ok(_) => println!("Song change detected, stopping playback."),
-                        Err(_) => println!("Error: Could not pause the player."),
-                    }
-                    break;
-                }
+            Ok(Event::PlayerShutDown) => {
+                println!("Player has been shut down.");
+                break;
             }
+            Ok(Event::TrackChanged(_)) => {
+                match player.pause() {
+                    Ok(_) => println!("Song change detected, stopping playback."),
+                    Err(_) => println!("Error: Could not pause the player."),
+                }
+                break;
+            }
+            Ok(_) => continue,
             Err(err) => {
                 println!("D-Bus error: {}. Aborting.", err);
                 break;
             }
         }
     }
+    Ok(())
 }
 
 #[tokio::main]
@@ -64,16 +55,17 @@ async fn main() {
 
     let result = timeout(
         Duration::from_secs(600),
-        task::spawn_blocking(|| {
-            stop_after_current();
+        task::spawn_blocking(|| match stop_after_current() {
+            Ok(_) => println!("Success."),
+            Err(e) => println!("Error: {}", e),
         }),
     )
     .await;
 
     match result {
-        Ok(_) => println!("Done."),
+        Ok(_) => println!("Exiting program."),
         Err(_) => {
-            println!("Timeout reached.");
+            println!("Timeout reached. Exiting program.");
             std::process::exit(0);
         }
     }
